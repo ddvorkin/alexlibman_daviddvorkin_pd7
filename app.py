4df7a40bb7a05b3d6ee2ce8400fe57d4d2e303a1
Alex Libman <kingalex11235@yahoo.com>
2014-10-30 13:04:45 -0400

    app update

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 #Alex Libman, David Dvorkin pd.7 Soft Dev
-from flask import Flask
-import pymongo import Connection
+from flask import Flask, render_template, request
+from pymongo import Connection
+
+app = Flask(__name__)
 
 @app.route("/",methods = ["POST","GET"])
 def home():
-    pass
+    if request.method == "POST":
+        conn = Connection()
+        db = conn['login']
+        list = []
+        
+        #validate info from text boxes
+    else:
+        return render_template("login.html");
 
 @app.route("/register",methods = ["POST","GET"])
 def register():

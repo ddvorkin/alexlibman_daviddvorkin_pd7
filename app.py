3d2c78d8fd28e37d2722179d91bd4211ac1a1630
Alex Libman <kingalex11235@yahoo.com>
2014-10-31 13:20:10 -0400

    registration db code implemented

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #Alex Libman, David Dvorkin pd.7 Soft Dev
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import Connection
 
 app = Flask(__name__)
 
 @app.route("/",methods = ["POST","GET"])
 def home():
+    if request.form['b'] == "Register":
+        return redirect(url_for("register"))
     if request.method == "POST":
         conn = Connection()
         db = conn['login']
@@ -14,13 +16,26 @@ def home():
         list = [{user:pword}]
         db.users.insert(list)
         #validate info from text boxes
+        for a in db.users.find():
+            print a
+        return render_template("login.html")
     else:
-        return render_template("login.html");
+        return render_template("login.html")
 
 @app.route("/register",methods = ["POST","GET"])
 def register():
-    pass
+    if request.method == "POST":
+        conn = Connection()
+        db = conn['login']
+        user = request.form["username"]
+        pword = request.form["password"]
+        list = [{user:pword}]
+        db.users.insert(list)
+        return render_template("register.html")
+    else:
+        return render_template("register.html")
 
 if __name__=="__main__":
     app.debug = True
     app.run()
+

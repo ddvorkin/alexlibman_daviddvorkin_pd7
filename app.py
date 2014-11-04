9ffac6953ee7432559de25f08fa4c79c6a9736eb
Alex Libman <kingalex11235@yahoo.com>
2014-11-04 18:26:02 -0500

    seperated mongo from flask, login working along with all checks

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Alex Libman, David Dvorkin pd.7 Soft Dev
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import Connection
+from flask import Flask, render_template, request, redirect, url_for, session
+import login
 
 app = Flask(__name__)
 
@@ -10,64 +10,43 @@ def home():
     if request.method == "POST":
         if request.form['b'] == "Register":
             return redirect(url_for("register"))
-        conn = Connection()
-        db = conn['login']
         user = request.form["username"]
         pword = request.form["password"]
-        list = [{user:pword}]
-        db.users.insert(list)
-        #validate info from text boxes
-        for a in db.users.find():
-            print a
-        return render_template("login.html")
+        msg = login.login(user,pword)
+        if msg == "Successfully logged in.":
+            session["user"] = user
+            return redirect(url_for("inform"))
+            #redirect to info page
+        else:
+            return render_template("login.html",message=msg)
     else:
         return render_template("login.html")
 
 @app.route("/register",methods = ["POST","GET"])
 def register():
     if request.method == "POST":
-        conn = Connection()
-        db = conn['login']
         user = request.form["username"]
         pword = request.form["password"]
         pword2 = request.form["confirm_password"]
         name = request.form["name"]
-        if user == "":
-            msg = "Please enter a username."
+        msg = login.register(user,pword,pword2,name)
+        if msg == "Successfully registered.":
+            return redirect(url_for("home"))
+        else:
             return render_template("register.html",message=msg)
-        if next(db.users.find({"user":user}),None) != None:
-            msg = "The username entered is already registered."
-            return render_template("register.html",message=msg)
-        if pword == "" or pword2 == "":
-            msg = "No password entered in one or more of the fields."
-            return render_template("register.html",message=msg)
-        if pword != pword2:
-            msg = "Passwords entered do not match."
-            return render_template("register.html",message=msg)
-        if name == "":
-            msg = "No name entered."
-            return render_template("register.html",message=msg)
-        list = [{"user":user,"password":pword,"name":name}]
-        db.users.insert(list)
-        for r in db.users.find({},{'_id':False}):
-            print r
-        return redirect(url_for("home"))
     else:
         return render_template("register.html")
 
-#@app.route("/mainpage", methods = ["POST", "GET"])
-#def inform():
-#    if request.method == "POST":
-#        conn = Connection()
-#        db = conn['login']
-#        list = [{user:pword}]
-#        db.users.insert(list)
-#        return redirect(url_for("/"))
-#    else:
-#        return render_template("mainpage.html")
-#FIX
+@app.route("/mainpage")
+def inform():
+    if "user" in session:
+        info = login.getinfo(session["user"])#dict which includes "user" and "name"
+        return render_template("mainpage.html",info=info)
+    else:
+        return render_template("mainpage.html",msg="You must be logged in to view the contents of this page.") #???
 
 if __name__=="__main__":
     app.debug = True
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run()
 
